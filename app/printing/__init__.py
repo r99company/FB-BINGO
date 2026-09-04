cf05bdd460789560b242aaeeda5412228a6f48b5
r99company <r.99company@gmail.com>
2026-09-03 21:37:50 -0600

    Expone exportador PDF A4

--- app/printing/__init__.py
+++ app/printing/__init__.py
@@ -1,13 +1,18 @@
 from .bulk_exporter import BulkA4ExportResult, BulkA4SvgExporter
+from .bulk_pdf_exporter import BulkA4PdfExportResult, BulkA4PdfExporter
 from .layout import A4PrintLayout, A4SeriesLayout, CardPlacement, CardSlot, PrintSlot, PrintStyle
+from .pdf_renderer import A4PdfRenderer
 from .svg_renderer import A4SvgRenderer
 
 __all__ = [
     "A4PrintLayout",
     "A4SeriesLayout",
+    "A4PdfRenderer",
     "A4SvgRenderer",
     "BulkA4ExportResult",
     "BulkA4SvgExporter",
+    "BulkA4PdfExportResult",
+    "BulkA4PdfExporter",
     "CardPlacement",
     "CardSlot",
     "PrintSlot",
