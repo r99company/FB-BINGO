09453c4c7672256d14eeaecf0209711fad09c0ed
r99company <r.99company@gmail.com>
2026-09-03 21:27:40 -0600

    feat: expone exportacion masiva A4

--- app/printing/__init__.py
+++ app/printing/__init__.py
@@ -1,3 +1,4 @@
+from .bulk_exporter import BulkA4ExportResult, BulkA4SvgExporter
 from .layout import A4PrintLayout, A4SeriesLayout, CardPlacement, CardSlot, PrintSlot, PrintStyle
 from .svg_renderer import A4SvgRenderer
 
@@ -5,6 +6,8 @@ __all__ = [
     "A4PrintLayout",
     "A4SeriesLayout",
     "A4SvgRenderer",
+    "BulkA4ExportResult",
+    "BulkA4SvgExporter",
     "CardPlacement",
     "CardSlot",
     "PrintSlot",
