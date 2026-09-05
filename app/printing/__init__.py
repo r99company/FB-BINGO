2fa02749ee2bce8c15d9c1dd4449da3042846d8f
r99company <r.99company@gmail.com>
2026-09-05 15:29:23 -0600

    refactor: usa renderer moderno en la API de impresion

--- app/printing/__init__.py
+++ app/printing/__init__.py
@@ -1,10 +1,13 @@
 from .layout import A4PrintLayout, A4SeriesLayout, CardPlacement, CardSlot, PrintSlot, PrintStyle
-from .svg_renderer import A4SvgRenderer
+from .modern_svg_renderer import ModernA4SvgRenderer
+
+A4SvgRenderer = ModernA4SvgRenderer
 
 __all__ = [
     "A4PrintLayout",
     "A4SeriesLayout",
     "A4SvgRenderer",
+    "ModernA4SvgRenderer",
     "CardPlacement",
     "CardSlot",
     "PrintSlot",
