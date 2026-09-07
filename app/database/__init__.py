c397a9b07988234e82eaf8c144965e71f64fcd15
r99company <r.99company@gmail.com>
2026-09-06 19:12:30 -0600

    chore: export game history repository

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,3 +1,4 @@
+from .game_repository import SQLiteGameHistoryRepository
 from .series_repository import SQLiteSeriesRepository
 
-__all__ = ["SQLiteSeriesRepository"]
+__all__ = ["SQLiteGameHistoryRepository", "SQLiteSeriesRepository"]
