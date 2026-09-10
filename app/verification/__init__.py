6cd62f903a9126f7aa1748336ef1eebbc8d4ee8c
r99company <r.99company@gmail.com>
2026-09-10 18:18:23 -0500

    Export live prize monitoring services

--- app/verification/__init__.py
+++ app/verification/__init__.py
@@ -1,10 +1,13 @@
 from .check import CardCheckService, VerificationResult
+from .live_prizes import LivePrize, LivePrizeTracker
 from .service import VerificationRecord, VerificationService
 from .verifier import CardVerifier
 
 __all__ = [
     "CardCheckService",
     "CardVerifier",
+    "LivePrize",
+    "LivePrizeTracker",
     "VerificationRecord",
     "VerificationResult",
     "VerificationService",
