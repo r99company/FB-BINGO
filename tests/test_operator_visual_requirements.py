17b617d18e1d5d6aa728df338319955c9cba900e
r99company <r.99company@gmail.com>
2026-09-11 20:24:26 -0500

    test: corregir referencia de la pantalla operativa

--- tests/test_operator_visual_requirements.py
+++ tests/test_operator_visual_requirements.py
@@ -6,7 +6,7 @@ os.environ.setdefault("QT_QPA_PLATFORM", "offscreen")
 
 from PySide6.QtWidgets import QApplication
 
-from app.ui.main_window import BingoMainWindow
+from app.ui.main import BingoMainWindow
 
 
 def test_operator_board_uses_circular_ball_style_and_hidden_digitizer() -> None:
